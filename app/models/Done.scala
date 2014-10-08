acab804962757bcc7adcaaf93af26a45df8358db
mikebochenek <mike.bochenek@gmail.com>
2014-10-08 23:30:05 +0200

    oooppss, I was using test.done incorrectly as a base, but I should have used test.item

--- app/models/Done.scala
+++ app/models/Done.scala
@@ -9,37 +9,38 @@ import anorm.SqlParser._
 import scala.language.postfixOps
 
 /**
- * mysql> describe done
- * -> ;
- * +------------+--------------+------+-----+---------+----------------+
- * | Field      | Type         | Null | Key | Default | Extra          |
- * +------------+--------------+------+-----+---------+----------------+
- * | id         | int(11)      | NO   | PRI | NULL    | auto_increment |
- * | owner      | int(11)      | YES  |     | NULL    |                |
- * | donetext   | varchar(255) | YES  |     | NULL    |                |
- * | donedate   | int(11)      | YES  |     | NULL    |                |
- * | createdate | int(11)      | YES  |     | NULL    |                |
- * | deleted    | tinyint(1)   | YES  |     | NULL    |                |
- * +------------+--------------+------+-----+---------+----------------+
- * 6 rows in set (0.04 sec)
- *
+ * mysql> describe item;
+ * +------------+------------+------+-----+-------------------+----------------+
+ * | Field      | Type       | Null | Key | Default           | Extra          |
+ * +------------+------------+------+-----+-------------------+----------------+
+ * | id         | int(11)    | NO   | PRI | NULL              | auto_increment |
+ * | owner      | int(11)    | YES  |     | NULL              |                |
+ * | donetext   | text       | YES  |     | NULL              |                |
+ * | donedate   | datetime   | YES  |     | NULL              |                |
+ * | createdate | timestamp  | NO   |     | CURRENT_TIMESTAMP |                |
+ * | deleted    | tinyint(1) | YES  |     | NULL              |                |
+ * | category   | bigint(20) | NO   |     | NULL              |                |
+ * | doneDay    | bigint(20) | NO   |     | NULL              |                |
+ * +------------+------------+------+-----+-------------------+----------------+
+ * 8 rows in set (0.00 sec)
  */
-case class Done(id: Long, owner: Long, donetext: String, donedate: Long, createdate: Long, deleted: Boolean)
+case class Done(id: Long, owner: Long, donetext: String, donedate: Long,
+  createdate: Long, deleted: Boolean, category: Long, doneDay: Long)
 
 object Done {
-  // -- Parsers
 
-  /**
-   * Parse a Done from a ResultSet
-   */
+  /** Parse a Done from a ResultSet */
   val simple = {
-      get[Long]("done.id") ~
+    get[Long]("done.id") ~
       get[Long]("done.owner") ~
       get[String]("done.donetext") ~
       get[Long]("done.donedate") ~
       get[Long]("done.createdate") ~
-      get[Boolean]("user.deleted") map {
-        case id ~ owner ~ donetext ~ donedate ~ createdate ~ deleted => Done(id, owner, donetext, donedate, createdate, deleted)
+      get[Boolean]("done.deleted") ~
+      get[Long]("done.category") ~
+      get[Long]("done.doneDay") map {
+        case id ~ owner ~ donetext ~ donedate ~ createdate ~ deleted ~ category ~ doneDay 
+          => Done(id, owner, donetext, donedate, createdate, deleted, category, doneDay)
       }
   }
 
