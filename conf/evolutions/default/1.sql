b9c7d573fc104be91527654dadf62e6ab2d3420b
mikebochenek <mike.bochenek@gmail.com>
2014-10-10 16:07:10 +0200

    updating create table script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,6 +2,36 @@
 
 # --- !Ups
 
+delimiter $$
+
+CREATE TABLE `done` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` int(11) DEFAULT NULL,
+  `donetext` text,
+  `donedate` datetime DEFAULT NULL,
+  `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` tinyint(1) DEFAULT NULL,
+  `category` bigint(20) NOT NULL,
+  `doneDay` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1$$
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL,
+  `createdate` datetime DEFAULT NULL,
+  `lastlogindate` datetime DEFAULT NULL,
+  `deleted` bit(1) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `settings` longtext,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `openidtoken` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+
+
 create table user (
   email                     varchar(255) not null primary key,
   name                      varchar(255) not null,
