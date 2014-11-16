8d143360a3c45402b2305703be9624b8480aba72
mikebochenek <mike.bochenek@gmail.com>
2014-11-16 12:48:41 +0100

    adding admin screens

---
+++ app/controllers/Admin.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import java.io.File
+
+import play.Play
+import play.api.mvc.Action
+import play.api.mvc.Session
+import play.api.mvc.Controller
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models._
+import views._
+
+object Admin extends Controller with Secured {
+  def index() = IsAuthenticated { username =>
+    implicit request => {
+      val id = User.findByEmail(username).id
+      if (username.contains("bochenek")) {
+        Ok(views.html.iadmin())
+      } else {
+        Ok(views.html.index())
+      }
+    }
+  }
+}
